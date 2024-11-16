13c7e9585981675b24d40abb967e33b8329a73ff
rebeca13todo <rebeca.torrecilla@estudiantat.upc.edu>
2024-11-16 15:21:45 +0100

    Update distancia.py

--- distancia.py
+++ distancia.py
@@ -3,4 +3,8 @@ import participant as p
 
 def distancia(p1:p.Participant, p2:p.Participant):
     '''Recibe dos participantes y devuelve una distancia que es menor cuanto más similares/afines son'''
+
+    atr_iguals(p1.age, p2.age, 1)
     ...
+
+def atr_iguals(p, r, punt: int) -> int:
