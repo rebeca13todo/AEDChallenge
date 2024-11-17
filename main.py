160ddea3d5b5d606a483319ed869cbe9b75dcc9e
ANGELMC113 <angelmoralescirera@gmail.com>
2024-11-17 11:48:22 +0100

    FULL IN

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import participant as p
 path:str = "data/datathon_participants.json"
 participants = p.load_participants(path)
 
-print(participants)
+print(len(participants))
+
+#print(participants)
